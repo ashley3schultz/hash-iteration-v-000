71b641dc73c5ea6a294ec64b955d01ae92765fb2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-24 00:19:42 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|name,  age|
-  
+  birthday_kids.each {|name,  age| "Happy Birthday #{name}! You are now #{age} years old!"}
+  end
 end
 
 
